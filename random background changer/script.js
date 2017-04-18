f109bc806a50049d7697ff7240fec877fddf0a60
wiwum <wgward@gmail.com>
2017-04-18 13:41:23 -0500

    Update script.js

--- random background changer/script.js
+++ random background changer/script.js
@@ -1,11 +1,23 @@
 var colorSelect;
-var BgChanger = setTimeout(changeBg, 1000);
+var BgChanger = setInterval(changeBg, 1000);
 function changeBg () {
   colorSelect = Math.floor(Math.random() * 6) + 1;
   if (colorSelect == 1) {
     document.body.style.backgroundColor = "red";
   }
-  if (colorSelect > 5) {
+  if (colorSelect == 2) {
     document.body.style.backgroundColor = "yellow";
+  }
+  if (colorSelect == 3) {
+    document.body.style.backgroundColor = "green";
+  }
+  if (colorSelect == 4) {
+    document.body.style.backgroundColor = "purple";
+  }
+  if (colorSelect == 5) {
+    document.body.style.backgroundColor = "white";
+  }
+  if (colorSelect == 6) {
+    document.body.style.backgroundColor = "blue";
   }
 }
