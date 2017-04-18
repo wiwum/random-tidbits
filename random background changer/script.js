0bede45cab6d71c0da435cf8769271bbb84a47e1
wiwum <wgward@gmail.com>
2017-04-18 13:38:59 -0500

    Update script.js

--- random background changer/script.js
+++ random background changer/script.js
@@ -2,5 +2,10 @@ var colorSelect;
 var BgChanger = setTimeout(changeBg, 1000);
 function changeBg () {
   colorSelect = Math.floor(Math.random() * 6) + 1;
-  console.log(colorSelect);
+  if (colorSelect == 1) {
+    document.body.style.backgroundColor = "red";
+  }
+  if (colorSelect > 5) {
+    document.body.style.backgroundColor = "yellow";
+  }
 }
